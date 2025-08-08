bf938bb9d5b93edf845fb6ce2d4f0fe02146b6e9
DELL <govindgautam933@gmail.com>
2025-08-08 20:26:34 +0530

    made few changes

--- index.js
+++ index.js
@@ -312,7 +312,7 @@ async function getOrCreateRazorpayPlan(planKey) {
   const plan = await razorpay.plans.create({
     period:   cfg.period === "yearly" ? "year" : "month",
     interval: cfg.interval,
-    addon_applicable: 1,
+    // addon_applicable: 1,
     item: {
       name:        cfg.name,
       amount:      cfg.amountPaise,
